2dcc7eec1b0f5b0f0cf8f2e54a0240da6b5ee6c7
ahmedmostafa23 <72565217+ahmedmostafa23@users.noreply.github.com>
2023-07-27 14:49:47 +0300

    Add files via upload
    allowed API to be accessible from anywhere

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from TaylorLM import TaylorLM
 
 app = FastAPI()
+
+# this lets the API be accessible from anywhere.
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["*"],
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
+
 model_wrapper = TaylorLM()
 
 @app.get("/")
